adfe5d6e8cf0f2ba3fd81e67664ed59826eaa66d
WalentynCzernyszow <valiantsin.czernyshou@codilime.com>
2021-12-06 22:46:14 +0100

    few fixes

--- src/containers/Quiz/Quiz.js
+++ src/containers/Quiz/Quiz.js
@@ -89,7 +89,7 @@ class Quiz extends Component {
     return (
       <div className={classes.Quiz}>
         <div className={classes.QuizWrapper}>
-          <h1>Ответьте на все вопросы</h1>
+          <h1>Answer on all questions</h1>
 
           {
             this.state.loading
